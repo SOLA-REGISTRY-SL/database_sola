310d9ccb35123f12d38ecd869b7eec5d4d3f2f56
Momodu Deen Swarray <smartboytk2@gmail.com>
2016-03-30 12:17:14 +0000

    Edited the survey scripts

--- changeset/06_cadastre_survey_plan_details.sql
+++ changeset/06_cadastre_survey_plan_details.sql
@@ -5,40 +5,27 @@ DROP TABLE cadastre.survey_plan_details;
 CREATE TABLE cadastre.survey_plan_details
 (
   id character varying(20) NOT NULL,
-  "LSNo" character varying(20) NOT NULL,
-  "NameofOwner" character varying(50) NOT NULL, -- The name of the Property Owner/Applicant
-  "Gender" character varying(15) NOT NULL, -- The Gender (male/female) of the Applicant
-  "DateofBirth" date NOT NULL,
-  "PlaceofBirth" character varying(50),
-  "PhoneNo" character varying(50) NOT NULL,
-  "EmailAddress" character varying(60),
-  "NationalIDNo" character varying(30), -- National Identity Card No of the Owner/Applicant
-  "PassportNo" character varying(25), -- Passport No of the applicant is available.
-  "AddressofOwner" character varying(100) NOT NULL,
-  "PropertyNameofStreet" character varying(50) NOT NULL, -- The Name of the Street/Road where the land is situated. (eg. PENINSULA ROAD)
-  "PropertyAddressNo" character varying(10) NOT NULL, -- The address no of the property (eg. No. 2)
-  "TypeofLand" character varying(25) NOT NULL, -- The type of land (eg. Private Land, State Land)
-  "AreaofLand" double precision NOT NULL,
-  "LandMeasurement" character varying(30) NOT NULL,
-  "NameofLicenseSurveyor" character varying(50) NOT NULL, -- Name of the Licensed Surveyor who surveyed the land
-  "EastNeighborPlotHolder" character varying(50), -- Name of the person occupying the land on the east side of this property.
-  "WestNeighborPlotHolder" character varying(50), -- Name of the person occupying the land on the west side of this property.
-  "NorthNeighborPlotHolder" character varying(50), -- Name of the person occupying the land on the north side of this property.
-  "SouthNeighborPlotHolder" character varying(50), -- Name of the person occupying the land on the sourth side of this property.
-  "SurveyingMethod" character varying(50) NOT NULL,
-  "DirectorofSurveys" character varying(50),
-  "DateSurveyed" date NOT NULL,
-  "BeaconNumber" character varying(30) NOT NULL,
-  "NameofCO" character varying(50), -- Name of the Charting Officer
-  "PhoneNoofCO" character varying(30),
-  "DateApprovedbyCO" date, -- The date that this survey plan was approved by the Charting Officer (CO).
-  "DesignationofCO" character varying(30), -- The designation/position of the Charting Officer.
-  "NameofSLCO" character varying(50), -- The Name of the State Land Clearing Officer (SLCO)
-  "PhoneNoOfSLCO" character varying(30),
-  "EmailAddressOfSLCO" character varying(50),
-  "DateInputtedIntoSystem" date, -- Date the survy plan was inputted into the system.
-  "InputtedBy" character varying(30), -- The user that inputted this record into the SOLA system.
-  CONSTRAINT "License Surveyor No" PRIMARY KEY ("LSNo"),
+  "ls_nr" character varying(20) NOT NULL,
+  "owner_name" character varying(50) NOT NULL, -- The name of the Property Owner/Applicant
+  "name_of_street" character varying(50) NOT NULL, -- The Name of the Street/Road where the land is situated. (eg. PENINSULA ROAD)
+  "address_nr" character varying(10) NOT NULL, -- The address no of the property (eg. No. 2)
+  "land_type" character varying(25) NOT NULL, -- The type of land (eg. Private Land, State Land)
+  "land_area" double precision NOT NULL,
+  "land_measurement" character varying(30) NOT NULL,
+  "license_surveyor_name" character varying(50) NOT NULL, -- Name of the Licensed Surveyor who surveyed the land
+  "east_neighbour" character varying(50), -- Name of the person occupying the land on the east side of this property.
+  "west_neighbour" character varying(50), -- Name of the person occupying the land on the west side of this property.
+  "north_neighbour" character varying(50), -- Name of the person occupying the land on the north side of this property.
+  "sourth_neighbour" character varying(50), -- Name of the person occupying the land on the sourth side of this property.
+  "survey_method" character varying(50) NOT NULL,
+  "director_of_survey" character varying(50),
+  "date_surveyed" date NOT NULL,
+  "beacon_number" character varying(30) NOT NULL,
+  "charting_officer_name" character varying(50), -- Name of the Charting Officer
+  "state_land_clearing_officer_name" character varying(50), -- The Name of the State Land Clearing Officer (SLCO)
+  "input_date" date, -- Date the survy plan was inputted into the system.
+  "inputed_by" character varying(30), -- The user that inputted this record into the SOLA system.
+  CONSTRAINT "License Surveyor No" PRIMARY KEY ("ls_nr"),
   CONSTRAINT "Foreign Key" FOREIGN KEY (id)
       REFERENCES cadastre.cadastre_object (id) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE
@@ -48,22 +35,17 @@ WITH (
 );
 ALTER TABLE cadastre.survey_plan_details
   OWNER TO postgres;
-COMMENT ON COLUMN cadastre.survey_plan_details."NameofOwner" IS 'The name of the Property Owner/Applicant';
-COMMENT ON COLUMN cadastre.survey_plan_details."Gender" IS 'The Gender (male/female) of the Applicant';
-COMMENT ON COLUMN cadastre.survey_plan_details."NationalIDNo" IS 'National Identity Card No of the Owner/Applicant';
-COMMENT ON COLUMN cadastre.survey_plan_details."PassportNo" IS 'Passport No of the applicant is available.';
-COMMENT ON COLUMN cadastre.survey_plan_details."PropertyNameofStreet" IS 'The Name of the Street/Road where the land is situated. (eg. PENINSULA ROAD)';
-COMMENT ON COLUMN cadastre.survey_plan_details."PropertyAddressNo" IS 'The address no of the property (eg. No. 2)';
-COMMENT ON COLUMN cadastre.survey_plan_details."NameofLicenseSurveyor" IS 'Name of the Licensed Surveyor who surveyed the land';
-COMMENT ON COLUMN cadastre.survey_plan_details."EastNeighborPlotHolder" IS 'Name of the person occupying the land on the east side of this property.';
-COMMENT ON COLUMN cadastre.survey_plan_details."WestNeighborPlotHolder" IS 'Name of the person occupying the land on the west side of this property.';
-COMMENT ON COLUMN cadastre.survey_plan_details."NorthNeighborPlotHolder" IS 'Name of the person occupying the land on the north side of this property.';
-COMMENT ON COLUMN cadastre.survey_plan_details."SouthNeighborPlotHolder" IS 'Name of the person occupying the land on the sourth side of this property.';
-COMMENT ON COLUMN cadastre.survey_plan_details."NameofCO" IS 'Name of the Charting Officer';
-COMMENT ON COLUMN cadastre.survey_plan_details."DateApprovedbyCO" IS 'The date that this survey plan was approved by the Charting Officer (CO).';
-COMMENT ON COLUMN cadastre.survey_plan_details."DesignationofCO" IS 'The designation/position of the Charting Officer.';
-COMMENT ON COLUMN cadastre.survey_plan_details."NameofSLCO" IS 'The Name of the State Land Clearing Officer (SLCO)';
-COMMENT ON COLUMN cadastre.survey_plan_details."DateInputtedIntoSystem" IS 'Date the survy plan was inputted into the system.';
-COMMENT ON COLUMN cadastre.survey_plan_details."InputtedBy" IS 'The user that inputted this record into the SOLA system.';
+COMMENT ON COLUMN cadastre.survey_plan_details."owner_name" IS 'The name of the Property Owner/Applicant';
+COMMENT ON COLUMN cadastre.survey_plan_details."name_of_street" IS 'The Name of the Street/Road where the land is situated. (eg. PENINSULA ROAD)';
+COMMENT ON COLUMN cadastre.survey_plan_details."address_nr" IS 'The address no of the property (eg. No. 2)';
+COMMENT ON COLUMN cadastre.survey_plan_details."license_surveyor_name" IS 'Name of the Licensed Surveyor who surveyed the land';
+COMMENT ON COLUMN cadastre.survey_plan_details."east_neighbour" IS 'Name of the person occupying the land on the east side of this property.';
+COMMENT ON COLUMN cadastre.survey_plan_details."west_neighbour" IS 'Name of the person occupying the land on the west side of this property.';
+COMMENT ON COLUMN cadastre.survey_plan_details."north_neighbour" IS 'Name of the person occupying the land on the north side of this property.';
+COMMENT ON COLUMN cadastre.survey_plan_details."sourth_neighbour" IS 'Name of the person occupying the land on the sourth side of this property.';
+COMMENT ON COLUMN cadastre.survey_plan_details."charting_officer_name" IS 'Name of the Charting Officer';
+COMMENT ON COLUMN cadastre.survey_plan_details."state_land_clearing_officer_name" IS 'The Name of the State Land Clearing Officer (SLCO)';
+COMMENT ON COLUMN cadastre.survey_plan_details."input_date" IS 'Date the survy plan was inputted into the system.';
+COMMENT ON COLUMN cadastre.survey_plan_details."inputed_by" IS 'The user that inputted this record into the SOLA system.';
 
 
