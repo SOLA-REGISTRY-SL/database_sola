d2f889fad6c510137b72db46d8eaaad536f4c365
Momodu Deen Swarray <smartboytk2@gmail.com>
2016-04-15 11:30:20 +0000

    Attempting to pull in order to get the changes that moses have made

--- changeset/officers_script.sql
+++ changeset/officers_script.sql
@@ -1,7 +1,7 @@
 ﻿
 -- Table: party.party_type
 
-DELETE FROM party.party WHERE code = 'license_surveyor' OR code = 'charting_officer' OR code = 'state_land_clearing_officer';
+DELETE FROM party.party_type WHERE code = 'license_surveyor' OR code = 'charting_officer' OR code = 'state_land_clearing_officer';
 
 INSERT INTO party.party_type (code, display_value, status, description) values('license_surveyor','License Surveyor','c','A Certified License Surveyor of doing surveying of lands');
 INSERT INTO party.party_type (code, display_value, status, description) values('charting_officer','Charting Officer','c','A Certified Charting Officer');
