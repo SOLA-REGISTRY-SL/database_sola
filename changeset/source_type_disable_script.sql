d21ad606fbf0f87acbaf34d2b513cf994b86f403
Momodu Deen Swarray <smartboytk2@gmail.com>
2016-06-10 15:30:18 +0000

    Pushing Administrative_source_type disable script

---
+++ changeset/source_type_disable_script.sql
@@ -0,0 +1,16 @@
+﻿-- DISABLING DOCUMENT SOURCES THAT ARE NOT USED AND ARE NOT TO BE DISPLAYED
+-- IN THE LIST OF DOCUMENT THAT AN APPLICANT PROVIDES
+----------------------------------------------------------------------------
+ 
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'agreement';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'caveat';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'contractForSale';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'courtOrder';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'deed';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'lease';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'mortgage';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'powerOfAttorney';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'proclamation';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'requisition';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'title';
+UPDATE source.administrative_source_type SET status = 'x' WHERE code = 'will';
