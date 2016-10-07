98bd0a9dfb12d02683892704b93a1da6ef98d46f
unknown <RizzoM@NRC016.hq.un.fao.org>
2016-10-07 14:02:59 +0200

    new roles for clearance planning and environment

--- changeset/structure/changes_04.10.2016.sql
+++ changeset/structure/changes_04.10.2016.sql
@@ -73,14 +73,67 @@ COMMENT ON COLUMN cadastre.cadastre_object.environment_clearance IS 'Flag indica
 ALTER TABLE cadastre.cadastre_object_historic ADD COLUMN planning_clearance boolean;
 ALTER TABLE cadastre.cadastre_object_historic ADD COLUMN environment_clearance boolean;
 
+
+
+INSERT INTO system.appgroup(
+            id, name, description)
+    VALUES ('planning-group-id', 'Planning clearance', 'This group is meant to used for the Planning department who gives planning clearance');
+INSERT INTO system.appgroup(
+            id, name, description)
+    VALUES ('environment-group-id', 'Environment clearance', 'This group is meant to used for the Environment department who gives planning clearance');
+
 INSERT INTO system.approle (code, display_value, status, description) VALUES ('ManageSurveyPlan', 'Create/edit survey plans', 'c', 'Role used to create or edit survey plans.');
 INSERT INTO system.approle (code, display_value, status, description) VALUES ('PlanningClearance', 'Planing Clearance', 'c', 'Role used to give planing clearance for private land survey plan before approval.');
 INSERT INTO system.approle (code, display_value, status, description) VALUES ('EvironmentClearance', 'Environment Clearance', 'c', 'Role used to give environment clearance for private land survey plan before approval.');
+
+--- APPROLE SETTING FOR - planning-group-id
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnAssignSelf','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnEdit','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnStatus','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnUnassignSelf','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnValidate','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnView','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ChangePassword','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('DashbrdViewAssign','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('DashbrdViewUnassign','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('documentCopy','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ManageSecurity','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ManageSettings','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('NoPasswordExpiry','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('serviceEnquiry','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('SourceSearch','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('CompleteService','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('RevertService','planning-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ViewMap','planning-group-id');
+
+--- APPROLE SETTING FOR - environment-group-id
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnAssignSelf','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnEdit','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnStatus','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnUnassignSelf','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnValidate','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ApplnView','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ChangePassword','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('DashbrdViewAssign','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('DashbrdViewUnassign','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('documentCopy','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ManageSecurity','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ManageSettings','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('NoPasswordExpiry','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('serviceEnquiry','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('SourceSearch','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('CompleteService','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('RevertService','environment-group-id');
+INSERT INTO system.approle_appgroup (approle_code,appgroup_id) values('ViewMap','environment-group-id');
+
+---- NEW ROLES FOR MANAGESURVEYPLAN GIVEN TO CHARTINGGIS AND SUPER GROUPS
 INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('ManageSurveyPlan', 'charting-group-id');
 INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('ManageSurveyPlan', 'gis-group-id');
 INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('ManageSurveyPlan', 'super-group-id');
-INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('PlanningClearance', 'clearance-group-id');
-INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('EvironmentClearance', 'clearance-group-id');
+
+
+--INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('PlanningClearance', 'clearance-group-id');
+--INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('EvironmentClearance', 'clearance-group-id');
 
 --INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('PlanningClearance', 'super-group-id');
 --INSERT INTO system.approle_appgroup (approle_code, appgroup_id) VALUES ('EvironmentClearance', 'super-group-id');
@@ -97,10 +150,10 @@ INSERT INTO system.br_definition(br_id, active_from, active_until, body)
 
 
 INSERT INTO system.br_validation(br_id, target_code, target_service_moment, target_request_type_code, severity_code, order_of_execution)
-    VALUES ('survey-plan-cleared-by-planning', 'service', 'complete', 'newParcel', 'critical', 140);
+    VALUES ('survey-plan-cleared-by-planning', 'service', 'complete', 'newParcel', 'medium', 140);
 
 INSERT INTO system.br_validation(br_id, target_code, target_service_moment, target_request_type_code, severity_code, order_of_execution)
-    VALUES ('survey-plan-cleared-by-planning', 'service', 'complete', 'existingParcel', 'critical', 150);
+    VALUES ('survey-plan-cleared-by-planning', 'service', 'complete', 'existingParcel', 'medium', 150);
 
 
 INSERT INTO system.br(id, display_name, technical_type_code, feedback, description, technical_description)
@@ -115,9 +168,9 @@ INSERT INTO system.br_definition(br_id, active_from, active_until, body)
 
 
 INSERT INTO system.br_validation(br_id, target_code, target_service_moment, target_request_type_code, severity_code, order_of_execution)
-    VALUES ('survey-plan-cleared-by-environment', 'service', 'complete', 'newParcel', 'critical', 160);
+    VALUES ('survey-plan-cleared-by-environment', 'service', 'complete', 'newParcel', 'medium', 160);
 
 INSERT INTO system.br_validation(br_id, target_code, target_service_moment, target_request_type_code, severity_code, order_of_execution)
-    VALUES ('survey-plan-cleared-by-environment', 'service', 'complete', 'existingParcel', 'critical', 170);
+    VALUES ('survey-plan-cleared-by-environment', 'service', 'complete', 'existingParcel', 'medium', 170);
     
 
